86137937d5a4fb014ecfad0ff27a5aa4f1950772
Jaycode068 <ajayijohnson68@yahoo.com>
2023-06-24 08:38:19 +0100

    Developed test file for the views

--- test_views.py
+++ test_views.py
@@ -2,7 +2,7 @@ import pytest
 from flask import url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from ..app import app
+from .app import app
 from remote_doctor.models import Doctor, Patient, Appointment, MedicalRecord, User
 
 
